ae10bbd548891a2b16eebb287d700e1da9e6f643
Vincentius <vincentiussss@gmail.com>
2019-11-25 16:28:50 +0700

    no error, tapi ga keluar modal nya

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,17 +64,17 @@ class Header extends React.Component {
                     </Collapse>
                 </Navbar>
 
-                <Button color="danger" onClick={this.toggleModal}>Ashiap</Button>
-                <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>Modal title</ModalHeader>
-                    <ModalBody>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="primary" onClick={this.toggleModal}>Do Something</Button>{' '}
-                        <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
-                    </ModalFooter>
-                </Modal>
+                <Button color="danger" onClick={this.toggleModaltoggle}>Ashiap</Button>
+      <Modal isOpen={this.state.modal} toggle={this.toggleModaltoggle}>
+        <ModalHeader toggle={this.toggleModaltoggle}>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={this.toggleModaltoggle}>Do Something</Button>{' '}
+          <Button color="secondary" onClick={this.toggleModaltoggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
             </div>
         );
     }
