5de9994680da2e268afd04c4d54d7bf9f99f121d
Vincentius <vincentiussss@gmail.com>
2019-11-27 09:30:46 +0700

    Axios Post & Get (Submit&Delte)

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,94 @@
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 import { Alert } from "reactstrap";
+import Axios from 'axios';
+import {
+    Table,Button, Form
+  } from 'reactstrap';
 
 class Home extends Component{
+    state={
+        data:[]
+    }
+
+    componentDidMount(){
+        Axios.get('http://localhost:2000/users')
+        .then((res) => {
+            console.log(res.data)
+            this.setState({data:res.data})
+            console.log(this.state.data)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+
+
+    renderUserData = () =>{
+        return this.state.data.map((val, index)=>{
+            return(
+                <tr>
+                    <td>{index+1}</td>
+                    <td>{val.first_name}</td>
+                    <td>{val.last_name}</td>
+                    <td>{val.email}</td>
+                    <td>
+                        <Button onClick={()=> this.deleteDataSpesifik(val.id)}>Delete</Button> 
+                        <Button onClick={()=> this.editData(val.id)}>Edit</Button>
+                        </td>
+                </tr>
+            )
+        })
+    }
+
+    submitData=()=>{
+        var namaDepan = this.refs.namaDepan.value
+        var namaBelakang = this.refs.namaBelakang.value
+        var email = this.refs.email.value
+        Axios.post('http://localhost:2000/users', {
+            first_name: namaDepan,
+            last_name: namaBelakang,
+            email:email
+        })
+        .then((res) => {
+            console.log(res.data);
+            Axios.get('http://localhost:2000/users')
+            this.componentDidMount()
+            this.refs.namaDepan.value=''
+            this.refs.namaBelakang.value=''
+            this.refs.email.value=''            
+        })
+        .catch((err)=> {
+            console.log(err);
+        })
+    }
+
+    deleteData = () => {
+        var panjang = this.state.data.length;
+        console.log(panjang)
+        var url = `http://localhost:2000/users/${panjang}`
+        Axios.delete(url)
+        .then(res => {
+            this.componentDidMount()
+            console.log(res.data)
+        }).catch(error => {
+            console.log(error);
+        }); 
+    }
+    deleteDataSpesifik = (a) => {
+        var url=`http://localhost:2000/users/${a}`
+        Axios.delete(url)
+        .then(res => {
+            this.componentDidMount()
+            console.log(res.data)
+        }).catch(error => {
+            console.log(error);
+        }); 
+    }
+
     render(){
         return(
-            <div>
+            <div >
                 <Alert color="primary">
                     Ini home!
                 </Alert>
@@ -15,6 +98,31 @@ class Home extends Component{
                     </button>
                     
                 </Link>
+                    <Table hover className="text-center">
+                        <thead >
+                            <tr>
+                                <th>No</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody >
+                            {this.renderUserData()}
+                        </tbody>
+                    </Table>
+                    <Form>
+                    <input type="text" className='form-control' ref='namaDepan'></input>
+                    <input type="text" className='form-control' ref='namaBelakang'></input>
+                    <input type="email" className='form-control' ref='email'></input>
+                    <Button color='primary' onClick={this.submitData}>
+                        Submit
+                    </Button>
+                    <Button color='danger' onClick={this.deleteData}>
+                        Delete
+                    </Button>
+                    </Form>
             </div>
         )
     }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,13 +10,12 @@ import {
     UncontrolledDropdown,
     DropdownToggle,
     DropdownMenu,
-    DropdownItem, Button, Modal, ModalHeader, ModalBody, ModalFooter
+    DropdownItem
 } from 'reactstrap';
 
 class Header extends React.Component {
     state = {
-        isOpen: false,
-        modal: false
+        isOpen: false
     };
     toggle() {
         this.setState({
@@ -24,15 +23,11 @@ class Header extends React.Component {
         });
     }
 
-    toggleModal() {
-        this.setState({
-            modal: !this.state.modal
-        });
-    }
+   
     render() {
         return (
             <div>
-                <Navbar color="light" light expand="md">
+                <Navbar color="dark" light expand="md">
                     <NavbarBrand href="/">Reactstrap</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
@@ -64,17 +59,6 @@ class Header extends React.Component {
                     </Collapse>
                 </Navbar>
 
-                <Button color="danger" onClick={this.toggleModaltoggle}>Ashiap</Button>
-      <Modal isOpen={this.state.modal} toggle={this.toggleModaltoggle}>
-        <ModalHeader toggle={this.toggleModaltoggle}>Modal title</ModalHeader>
-        <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={this.toggleModaltoggle}>Do Something</Button>{' '}
-          <Button color="secondary" onClick={this.toggleModaltoggle}>Cancel</Button>
-        </ModalFooter>
-      </Modal>
             </div>
         );
     }
